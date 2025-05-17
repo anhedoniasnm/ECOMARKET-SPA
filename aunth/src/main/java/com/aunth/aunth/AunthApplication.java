045ddefc4ce1a93aae6ba37a358dcf9ecd02fda6
Simón <simon.tobar98l@gmail.com>
2025-05-17 16:57:52 -0400

    Microservicios añadidos
    auntenticación, empleados, inventario, logistica, pedidos, proveedores, sucursal y ventas, completando los 10 microservicios a trabajar.

---
+++ aunth/src/main/java/com/aunth/aunth/AunthApplication.java
@@ -0,0 +1,13 @@
+package com.aunth.aunth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AunthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AunthApplication.class, args);
+	}
+
+}
