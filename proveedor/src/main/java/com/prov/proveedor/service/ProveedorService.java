d73a713d46d637ada88589a0759f522996c3ba51
Simón <simon.tobar98l@gmail.com>
2025-05-22 22:44:56 -0400

    Clases Proveedor
    se terminan las clases de proveedor

---
+++ proveedor/src/main/java/com/prov/proveedor/service/ProveedorService.java
@@ -0,0 +1,11 @@
+package com.prov.proveedor.service;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class ProveedorService {
+
+}
