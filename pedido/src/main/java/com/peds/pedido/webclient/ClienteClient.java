0b3f17c9c7b16f9ab5e476e23992ba2a4b9df64b
Simón <simon.tobar98l@gmail.com>
2025-05-22 23:17:26 -0400

    Conexiones
    creado el webclient para el cliente + añadido la dependencia de webflux para todos los microservicios

---
+++ pedido/src/main/java/com/peds/pedido/webclient/ClienteClient.java
@@ -0,0 +1,33 @@
+package com.peds.pedido.webclient;
+
+import org.springframework.stereotype.Component;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.reactive.function.client.WebClient;
+import java.util.Map;
+
+@Component
+public class ClienteClient {
+
+    private final WebClient webClient;
+
+    public ClienteClient(@Value("${cliente-service.url}") String clienteServiceUrl){
+        this.webClient = WebClient.builder()
+            .baseUrl(clienteServiceUrl)
+            .build();
+    }
+
+    public Map<String, Object> getClienteById(Long id){
+        return this.webClient.get()
+        .uri("/{id}", id)
+        .retrieve()
+        .onStatus(
+            status -> status.is4xxClientError(),
+            response -> response.bodyToMono(String.class)
+            .map(body -> new RuntimeException("Cliente no encontrado (ID: " + id + ")"))
+        )
+        .bodyToMono(Map.class)
+        .block();
+    }
+
+}
