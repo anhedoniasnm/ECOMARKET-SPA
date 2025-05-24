b5f873bb459e90c8632949a9e9807dc07b03a78a
Simón <simon.tobar98l@gmail.com>
2025-05-23 22:57:44 -0400

    import fix

--- notificacion/src/main/java/com/notif/notificacion/controller/NotifController.java
+++ notificacion/src/main/java/com/notif/notificacion/controller/NotifController.java
@@ -1,5 +1,7 @@
 package com.notif.notificacion.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/notif")
