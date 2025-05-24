376df08614fd377d02db921fd79f9a07f73b696f
Simón <simon.tobar98l@gmail.com>
2025-05-23 21:59:14 -0400

    Mapeos del Cliente

--- cliente/src/main/java/com/ciente/cliente/model/Cliente.java
+++ cliente/src/main/java/com/ciente/cliente/model/Cliente.java
@@ -1,11 +1,15 @@
 package com.ciente.cliente.model;
 
+import org.hibernate.mapping.Join;
+
 import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,7 +35,8 @@ public class Cliente {
     @Column(nullable = false)
     private String contrasenha;
 
-    @Column
+    @ManyToOne
+    @Column(name = "pedido_id")
     private Pedido pedidos;
     
 
--- cliente/src/main/java/com/ciente/cliente/services/ClienteService.java
+++ cliente/src/main/java/com/ciente/cliente/services/ClienteService.java
@@ -1,11 +1,36 @@
 package com.ciente.cliente.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ciente.cliente.model.Cliente;
+import com.ciente.cliente.repository.ClienteRepository;
 
 import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
 public class ClienteService {
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> getClientes(){
+        return clienteRepository.findAll();
+    }
+
+    public Cliente getClienteById(Long id){
+        return clienteRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Cliente not found"));
+    }
+
+    public Cliente saveCliente(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public void deleteCliente(Long id){
+        clienteRepository.deleteById(id);
+    }
 
 }
