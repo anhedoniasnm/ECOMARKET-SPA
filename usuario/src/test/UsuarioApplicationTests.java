09f3f03588c706c5369ade6b102f70a73bdb400a
Simón <simon.tobar98l@gmail.com>
2025-06-23 23:25:47 -0400

    tests y packages

---
+++ usuario/src/test/UsuarioApplicationTests.java
@@ -0,0 +1,14 @@
+package com.user.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
